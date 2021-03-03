adf0e722048132c0ff8ea2a76fd3943ae8335ad5
Malte Rosenbjerg <rosenbjerg@users.noreply.github.com>
2021-03-04 00:56:06 +0100

    Cleanup

--- AgentDeploy.ExternalApi/Options/ExecutionOptions.cs
+++ AgentDeploy.ExternalApi/Options/ExecutionOptions.cs
@@ -2,7 +2,14 @@
 {
     public class ExecutionOptions
     {
+        /// <summary>
+        /// Which shell to execute the command file with
+        /// </summary>
         public string Shell { get; set; } = "bash";
+        
+        /// <summary>
+        /// Whether to replace C:\ in absolute path with /mnt/c/ and replace \ with / in paths
+        /// </summary>
         public bool UseWslPath { get; set; } = true;
     }
 }
