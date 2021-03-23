4acd485d274e0ae9ddff9a5e81e7b1efb36c9f24
Malte Rosenbjerg <rosenbjerg@users.noreply.github.com>
2021-03-24 00:56:50 +0100

    Client progress + ws support

---
+++ client/src/form-utils.js
@@ -0,0 +1,60 @@
+import path from 'path';
+import fs from 'fs';
+
+import FormData from 'form-data';
+
+const keyValuePairRegex = /^([a-zA-Z0-9-_]+)+=(.*)$/;
+export function createForm(command, options) {
+    const formdata = new FormData();
+    formdata.append('command', command);
+
+    let error = false;
+    error = addFieldToForm(options.variables || [], formdata, 'variable', 'Variable') || error;
+    error = addFieldToForm(options.secretVariables || [], formdata, 'secretVariable', 'Secret variable') || error;
+    error = addFieldToForm(options.environmentVariables || [], formdata, 'environment', 'Environment variable') || error;
+    error = addFileToForm(options.files || [], formdata) || error;
+
+    if (error) {
+        console.error(`Argument validation failed`);
+        process.exit(-1);
+    }
+
+    return formdata;
+}
+
+function addFieldToForm(collection, formdata, kind, formattedKind) {
+    let error = false;
+    for (const variable of collection){
+        if (!keyValuePairRegex.test(variable)){
+            console.error(`${formattedKind} is not in correct key-value-pair format ${keyValuePairRegex}`);
+            error = true;
+        }
+        else {
+            formdata.append(kind, variable);
+        }
+    }
+    return error;
+}
+
+function addFileToForm(collection, formdata) {
+    let error = false;
+    for (const fileArgument of collection) {
+        if (!keyValuePairRegex.test(fileArgument)){
+            console.error(`File argument is not in correct key-value-pair format ${keyValuePairRegex}`);
+            error = true;
+        }
+        else {
+            const match = fileArgument.match(keyValuePairRegex);
+            const key = match[1];
+            const filePath = match[2];
+            if (!fs.existsSync(filePath)) {
+                console.error(`Provided file does not exist: ${filePath}`);
+                error = true;
+            }
+            else {
+                formdata.append(key, fs.createReadStream(filePath), { filename: path.basename(filePath) });
+            }
+        }
+    }
+    return error;
+}
--- client/src/index.js
+++ client/src/index.js
@@ -1,138 +1,105 @@
-const fs = require('fs');
-const path = require('path');
-const fetch = require('node-fetch');
-const chalk = require('chalk');
-const { Command } = require('commander');
-const FormData = require('form-data');
+import fs from 'fs';
+
+import fetch from 'node-fetch';
+import chalk from 'chalk';
+import { Command } from 'commander';
+import { createForm } from './form-utils.js';
+import { v4 as uuidv4 } from 'uuid';
+import WebSocket from 'ws';
 
 const TokenFilePath = './agentd.token';
 const program = new Command();
 
 program
     .name('agentd client')
-    .version('0.0.1')
+    .version('0.0.2')
     .option('-t, --token <token>', 'Authentication token')
     .option('-v, --variables <keyValuePair...>', 'Add variable')
     .option('-s, --secret-variables <keyValuePair...>', 'Add secret variable')
     .option('-e, --environment-variables <keyValuePair...>', 'Add environment variable')
     .option('-f, --files <keyValuePair...>', 'Add file')
+    .option('--ws', 'enable websocket connection for receiving output')
     .command('invoke <command> <serverUrl>')
-    .description('Invoke command')
+    .description('Invoke named command on remote server')
     .action(invokeCommand);
+
+const fail = str => {
+    console.error(chalk.red(str));
+    process.exit(-1);
+};
+
+async function printValidationErrors(response) {
+    const json = await response.json();
+    const message = `${chalk.bold(json.message)}\n${json.errors.map(e => `${e.name} failed: ${e.error}`).join('\n')}`;
+    fail(message);
+}
+
+function printFormatted(output) {
+    const formatted = `${chalk.dim(output.timestamp.padEnd(29, ' ') + '|')} ${output.output}`;
+    if (output.error) console.log(chalk.red(formatted));
+    else console.log(formatted);
+}
+
+async function handleSuccessResponse(response) {
+    const json = await response.json();
+
+    if (json.command) {
+        console.log(`--- ${chalk.bold('Command')} -----------------------------------------------------------`);
+        console.log(json.command);
+    }
+
+    if (json.output && json.output.length) {
+        console.log(`--- ${chalk.bold('Output')} ------------------------------------------------------------`);
+        for (const output of json.output) {
+            printFormatted(output);
+        }
+    }
+
+    if (json.exitCode === 0) console.log(chalk.bold.green('Command executed successfully'));
+    else console.log(chalk.bold.red(`Command exited with non-zero exit code: ${json.exitCode}`));
+    process.exit(json.exitCode);
+}
 
 async function invokeCommand(command, serverUrl) {
     const options = program.opts();
-    if (!options.token && fs.existsSync(TokenFilePath)) {
-        options.token = fs.readFileSync(TokenFilePath, 'utf-8');
-    }
-    else if (!options.token) {
-        console.error(`Token must be provided by placing a file containing the token at the path ${TokenFilePath} or by using the token argument (-t)`);
-        process.exit(-1);
+    if (!options.token) {
+        if (fs.existsSync(TokenFilePath))
+            options.token = fs.readFileSync(TokenFilePath, 'utf-8');
+        else
+            fail(`Token must be provided by placing a file containing the token at the path ${TokenFilePath} or by using the token argument (-t)`);
     }
 
     const formdata = createForm(command, options);
-    const response = await fetch(`${serverUrl}/rest/invoke`, { method: 'POST', body: formdata, headers: { 'Authorization': `Token ${options.token}` } });
-
-    if (response.status === 404) {
-        console.error(`Command ${command} not found`);
-        process.exit(-1);
+    const websocketId = uuidv4();
+    if (options.ws) formdata.append('websocket-session-id', websocketId)
+    const responsePromise = fetch(`${serverUrl}/rest/invoke`, { method: 'POST', body: formdata, headers: { 'Authorization': `Token ${options.token}` } });
+    if (options.ws) {
+        setTimeout(() => {
+            const wsUrl = `${serverUrl}/websocket/connect/${websocketId}`.replace('https', 'wss').replace('http', 'ws');
+            const websocket = new WebSocket(wsUrl);
+            websocket.on('open', () => console.log(`--- ${chalk.bold('Output')} ------------------------------------------------------------`));
+            websocket.on('message', json => {
+                const msg = JSON.parse(json);
+                if (msg.event === 'output')
+                    printFormatted(msg.data);
+            });
+        }, 500);
     }
-    else if (response.status === 401) {
-        console.error(`Token is invalid`);
-        process.exit(-1);
-    }
-    else if (response.status === 400) {
-        const json = await response.json();
-        console.log(chalk.bold(json.message));
-        for (const error of json.errors) {
-            console.log(`${error.name} failed: ${error.error}`);
-        }
-        process.exit(-1);
-    }
-    else if (response.status === 200) {
-        const json = await response.json();
 
-        if (json.command) {
-            console.log(`--- ${chalk.bold('Command')} -----------------------------------------------------------`);
-            console.log(json.command);
-        }
-
-        if (json.output) {
-            console.log(`--- ${chalk.bold('Output')} ------------------------------------------------------------`);
-            for (const output of json.output) {
-                const formatted = `${chalk.dim(output.timestamp.padEnd(29, ' ') + '|')} ${output.output}`;
-                if (output.error) console.log(chalk.red(formatted));
-                else console.log(formatted);
-            }
-        }
-
-        if (json.exitCode === 0) console.log(chalk.bold.green('Command executed successfully'));
-        else console.log(chalk.bold.red(`Command exited with non-zero exit code: ${json.exitCode}`));
-        process.exit(json.exitCode);
-    }
-    else {
-        console.log('Unexpected status', response.status);
-        process.exit(-1);
+    const response = await responsePromise;
+    switch (response.status) {
+        case 404: return fail(`Command ${command} not found`);
+        case 401: return fail(`Token is invalid`);
+        case 400: return await printValidationErrors(response);
+        case 200: return await handleSuccessResponse(response);
+        default:
+            return fail(`Unexpected status: ${response.status}`);
     }
 }
 
-const keyValuePairRegex = /^([a-zA-Z0-9-_]+)+=(.*)$/;
-function createForm(command, options) {
-    const formdata = new FormData();
-    formdata.append('command', command);
 
-    let error = false;
-    error = addFieldToForm(options.variables || [], formdata, 'variable', 'Variable') || error;
-    error = addFieldToForm(options.secretVariables || [], formdata, 'secretVariable', 'Secret variable') || error;
-    error = addFieldToForm(options.environmentVariables || [], formdata, 'environment', 'Environment variable') || error;
-    error = addFileToForm(options.files || [], formdata) || error;
-
-    if (error) {
-        console.error(`Argument validation failed`);
-        process.exit(-1);
-    }
-
-    return formdata;
-}
-
-function addFieldToForm(collection, formdata, kind, formattedKind) {
-    let error = false;
-    for (const variable of collection){
-        if (!keyValuePairRegex.test(variable)){
-            console.error(`${formattedKind} is not in correct key-value-pair format ${keyValuePairRegex}`);
-            error = true;
-        }
-        else {
-            formdata.append(kind, variable);
-        }
-    }
-    return error;
-}
-
-function addFileToForm(collection, formdata) {
-    let error = false;
-    for (const fileArgument of collection) {
-        if (!keyValuePairRegex.test(fileArgument)){
-            console.error(`File argument is not in correct key-value-pair format ${keyValuePairRegex}`);
-            error = true;
-        }
-        else {
-            const match = fileArgument.match(keyValuePairRegex);
-            const key = match[1];
-            const filePath = match[2];
-            if (!fs.existsSync(filePath)) {
-                console.error(`Provided file does not exist: ${filePath}`);
-                error = true;
-            }
-            else {
-                formdata.append(key, fs.createReadStream(filePath), { filename: path.basename(filePath) });
-            }
-        }
-    }
-    return error;
-}
 
 async function main() {
-    program.parse(process.argv);
+    await program.parseAsync(process.argv);
 }
 main().catch(console.error);
