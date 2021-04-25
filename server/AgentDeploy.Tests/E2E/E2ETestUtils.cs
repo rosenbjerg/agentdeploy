561f278f8b0cb38015546b59126c9c57d7ccbf5c
Malte Rosenbjerg <rosenbjerg@users.noreply.github.com>
2021-04-25 13:44:16 +0200

    Autodiscover path to client src

--- server/AgentDeploy.Tests/E2E/E2ETestUtils.cs
+++ server/AgentDeploy.Tests/E2E/E2ETestUtils.cs
@@ -1,3 +1,6 @@
+using System;
+using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 using Instances;
 
@@ -5,7 +8,17 @@ namespace AgentDeploy.Tests.E2E
 {
     public static class E2ETestUtils
     {
-        private const string AgentdClientPath = "../../../../../client/src";
+        private static readonly string AgentdClientPath;
+
+        static E2ETestUtils()
+        {
+            var parts = Path.GetFullPath("./").Split(Path.DirectorySeparatorChar, StringSplitOptions.RemoveEmptyEntries);
+            var rootParts = parts.TakeWhile(part => part != "server");
+            var root = string.Join(Path.DirectorySeparatorChar, rootParts);
+            
+            AgentdClientPath = Path.Combine(root, "client", "src");
+        }
+        
         public static async Task<(int exitCode, Instance instance)> ClientOutput(string arguments)
         {
             return await Instance.FinishAsync("node", AgentdClientPath + " " +arguments);
