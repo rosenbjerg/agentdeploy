a1e9c2d67a5b8cff3ff5dde7fcaf2af8dc15a2fb
Malte Rosenbjerg <rosenbjerg@users.noreply.github.com>
2021-08-12 03:14:30 +0200

    Add some test logging on errors from backend

--- server/AgentDeploy.Tests/E2E/TestApiStartup.cs
+++ server/AgentDeploy.Tests/E2E/TestApiStartup.cs
@@ -1,10 +1,15 @@
+using System;
+using System.Diagnostics;
+using System.IO;
 using AgentDeploy.ExternalApi;
 using AgentDeploy.Models.Options;
 using AgentDeploy.Services;
 using AgentDeploy.Services.Scripts;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using Moq;
+using NUnit.Framework;
 
 namespace AgentDeploy.Tests.E2E
 {
@@ -18,12 +23,55 @@ namespace AgentDeploy.Tests.E2E
         {
             var mockScriptReader = new Mock<IScriptReader>();
             var mockTokenReader = new Mock<ITokenReader>();
-            
+
+            services.AddSingleton<ILoggerFactory, TestLoggerFactory>();
             services.AddSingleton<IFileService>(new FileService(new ExecutionOptions()));
             services.AddSingleton(mockScriptReader);
             services.AddSingleton(mockTokenReader);
             services.AddSingleton(mockScriptReader.Object);
             services.AddSingleton(mockTokenReader.Object);
+        }
+    }
+
+    public class TestLoggerFactory : ILoggerFactory
+    {
+        public void Dispose()
+        {
+        }
+
+        public void AddProvider(ILoggerProvider provider)
+        {
+        }
+
+        public ILogger CreateLogger(string categoryName)
+        {
+            return new TestLogger(categoryName);
+        }
+    }
+    public class TestLogger : ILogger
+    {
+        private readonly string _categoryName;
+
+        public TestLogger(string categoryName)
+        {
+            _categoryName = categoryName;
+        }
+        public IDisposable BeginScope<TState>(TState state)
+        {
+            return new MemoryStream();
+        }
+
+        public bool IsEnabled(LogLevel logLevel)
+        {
+            return logLevel == LogLevel.Error;
+        }
+
+        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+        {
+            if (exception == null) return;
+            Debug.WriteLine(exception.StackTrace);
+            Trace.WriteLine(exception.StackTrace);
+            TestContext.WriteLine(exception.StackTrace);
         }
     }
 }
