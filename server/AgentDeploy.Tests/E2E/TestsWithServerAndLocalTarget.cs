2d348f42bcb5afdc5d81978ba9f65e9742c1bb05
Malte Rosenbjerg <rosenbjerg@users.noreply.github.com>
2021-08-12 02:11:52 +0200

    Run local e2e test separately

--- server/AgentDeploy.Tests/E2E/TestsWithServerAndLocalTarget.cs
+++ server/AgentDeploy.Tests/E2E/TestsWithServerAndLocalTarget.cs
@@ -1,11 +1,8 @@
-using System.Threading.Tasks;
-using AgentDeploy.Models.Options;
-using Microsoft.Extensions.DependencyInjection;
 using NUnit.Framework;
 
 namespace AgentDeploy.Tests.E2E
 {
-    [Category("E2E")]
+    [Category("E2E-Local")]
     public class TestsWithServerAndLocalTarget : TestsWithServer
     {
         public TestsWithServerAndLocalTarget() : base(null)
